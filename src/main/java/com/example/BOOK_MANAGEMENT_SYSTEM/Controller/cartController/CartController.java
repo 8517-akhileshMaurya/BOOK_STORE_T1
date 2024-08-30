bbd3fde55d8013dc121657fb7844cc6356abcd08
Akhilesh Maurya <Akhilesh.Maurya@godigit.com>
2024-08-30 11:25:49 +0530

    AKhil commit

---
+++ src/main/java/com/example/BOOK_MANAGEMENT_SYSTEM/Controller/cartController/CartController.java
@@ -0,0 +1,22 @@
+package com.example.BOOK_MANAGEMENT_SYSTEM.Controller.cartController;
+
+import com.example.BOOK_MANAGEMENT_SYSTEM.Service.cartService.CartService;
+import com.example.BOOK_MANAGEMENT_SYSTEM.response.CartResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add")
+    public CartResponse addProductToCart(@RequestParam Long user_id, @RequestParam Long product_id, @RequestParam Integer quantity) {
+        return cartService.addProductToCart(user_id, product_id, quantity);
+    }
+}
+
---
+++ src/main/java/com/example/BOOK_MANAGEMENT_SYSTEM/model/wishlistModel/Wishlist.java
@@ -0,0 +1,27 @@
+package com.example.BOOK_MANAGEMENT_SYSTEM.model.wishlistModel;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long wishlist_id;
+
+    @Column(insertable=false, updatable=false)
+    private Long user_id;
+
+
+    @Column(nullable = false, name = "created_at")
+    private LocalDateTime created_at;
+
+    @OneToMany(mappedBy = "wishlist", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<wishlistItem> wishlistItemsList;
+
+}
